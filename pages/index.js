55b66ffc3793a8c6dd2479dda7de8be2a53d0743
anxhi <anxhirr@gmail.com>
2024-05-24 18:46:34 +0200

    feat: add shadcn ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>hello world</main>;
+  return (
+    <main>
+      <Button>Click me</Button>
+    </main>
+  );
 }
